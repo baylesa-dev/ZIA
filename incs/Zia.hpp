ea9980104059f01991c0559f45ac1d22e3c40f8b
Adrien 🍺 <adrien.bayles@epitech.eu>
2019-03-04 04:35:14 +0100

    Add config fil basic

--- incs/Zia.hpp
+++ incs/Zia.hpp
@@ -8,6 +8,18 @@
 #pragma once
 
 #include <thread>
+#include "API.hpp"
+#include "pugixml.hpp"
+#define VERSION "1.0.0"
+#define CONFIGPATH "config.yml"
+
+#ifdef __linux__
+#       define PLATFORM 0
+#elif _WIN32
+#       define PLATFORM 1
+#elif __APPLE__
+#       define PLATFORM 2
+#endif
 
 namespace Zia
 {
@@ -17,6 +29,8 @@ class Zia
   public:
     Zia();
     ~Zia();
+    void setConfig();
+    void printStartMessage();
     void startServer();
     void stopServer();
     void forceStopServer();
@@ -25,6 +39,7 @@ class Zia
   private:
     std::thread *_server;
     bool *_run;
+    API::ServerConfig _config;
 };
 
 } // namespace Zia
--- srcs/Zia.cpp
+++ srcs/Zia.cpp
@@ -1,5 +1,6 @@
 #include <thread>
 #include <iostream>
+#include <fstream>
 
 #include "Server.hpp"
 #include "Zia.hpp"
@@ -7,6 +8,8 @@
 Zia::Zia::Zia()
 {
     _run = (bool *)malloc(sizeof(bool));
+    setConfig();
+    printStartMessage();
 }
 
 Zia::Zia::~Zia()
@@ -15,6 +18,37 @@ Zia::Zia::~Zia()
         std::cout << "A server is running !" << std::endl;
         _server->join();
     }
+}
+
+void Zia::Zia::setConfig()
+{
+        pugi::xml_document config;
+
+        switch(PLATFORM)
+        {
+                case 0  : _config.platform = API::ServerConfig::Platform::Linux;        break;
+                case 1  : _config.platform = API::ServerConfig::Platform::Windows;      break;
+                case 2  : _config.platform = API::ServerConfig::Platform::Macos;        break;
+        }
+        pugi::xml_parse_result result = config.load_file("config.xml");
+        if (!result) {
+                std::cout << "config.xml: "<< result.description() << std::endl;
+                _config.name = "Default Zia";
+                _config.version = "0.0.0";
+                return ;
+        }
+        _config.name = config.child("server").attribute("name").value();
+        _config.version = config.child("server").attribute("version").value();
+}
+
+void Zia::Zia::printStartMessage()
+{
+        std::cout << "Server name : " << _config.name << std::endl;
+        std::cout << "Server Version :" << _config.version << std::endl << std::endl;
+        std::cout << " _______   _ " << std::endl;
+        std::cout << "|_  /_ _| /_\\"  << std::endl;
+        std::cout << " / / | | / _ \\"  << std::endl;
+        std::cout << "/___|___/_/ \\_\\" << std::endl << std::endl;
 }
 
 void Zia::Zia::startServer()
