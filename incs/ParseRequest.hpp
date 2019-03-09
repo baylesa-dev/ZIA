ab7a696bbb9248f3331ccf833f17659753106647
marius <marius.michelot@epitech.eu>
2019-03-09 05:54:19 +0100

    add file ParseRequest and add it to CMakeList

---
+++ incs/ParseRequest.hpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2019
+** Zia
+** File description:
+** ParseRequest
+*/
+
+#pragma once
+
+namespace Zia
+{
+
+class ParseRequest
+{
+  public:
+    ParseRequest();
+    ~ParseRequest();
+
+  protected:
+  private:
+};
+
+} // namespace Zia
---
+++ srcs/ParseRequest.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2019
+** ZIa
+** File description:
+** ParseRequest
+*/
+
+#include "ParseRequest.hpp"
+
+ParseRequest::ParseRequest()
+{
+}
+
+ParseRequest::~ParseRequest()
+{
+}
